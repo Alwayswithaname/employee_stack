b51099519a56e0b458d6e38cdb1fc0100739e3d0
whl <wplovitz@gmail.com>
2022-10-16 18:30:47 -0700

    added engineer car requests, and intern requests, built manager class

--- index.js
+++ index.js
@@ -68,16 +68,175 @@ function appMenu() {
                     },
                 },
             ])
-            .then((answer) => {
+            .then((answers) => {
                 const manager = new Manager(
-                    answer.managerName,
-                    answer.managerId,
-                    answer.managerEmail,
-                    answer.managerOfficeNumber
+                    answers.managerName,
+                    answers.managerId,
+                    answers.managerEmail,
+                    answers.managerOfficeNumber
                 );
                 teamMembers.push(manager);
                 idArray.push(answer.managerId);
                 creatTeam()
             })
     }
+    function creatTeam() {
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: "",
+                message: "",
+                choices: [
+                    'Engineer',
+                    'Intern',
+                    "I dont want to add any more team members",
+                ],
+            },
+        ])
+        .then((useerChoice) => {
+            switch (useerChoice.memberChoice) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                case 'intern':
+                    addIntern();
+                    break;
+                    default:
+                        buildTeam();
+            }
+        });
+    }
+    function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'engineerName',
+                    message: "What is the engineer's name?",
+                    validate: (answer) => {
+                        if (answer !== '') {
+                            return true;
+                        }
+                            return "Please enter at least on character.";
+                    },
+                },{
+                    type: 'input',
+                    name: 'engineerId',
+                    message: 'what is the engingeers ID number? ',
+                    validate: (answer) => {
+                        const pass = answer.match(/^[1-9]\d*$/)
+                        if (pass) {
+                            return true
+                        }
+                            return 'please enter a positive number greater than zero.'
+                    },
+                },{
+                    
+                        type: 'input',
+                        name: 'engineerEmail',
+                        message: "what is the engineer's email?",
+                        validate: (answer) => {
+                            const pass = answer.math(/\S+@\S+\.\S+/)
+                            if (pass) {
+                                return true
+                            }
+                                return 'please enter a vazlid email address.';
+                    },
+                },{
+                    type: 'input',
+                    name: 'engineerGithub',
+                    message: "What is this engineer's github username? ",
+                    validate: (answer) => {
+                        if (answer !== '') {
+                            return true
+                        }
+                            return 'please enter a positive number greater than zero.'
+                    },
+                },
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(
+                answers.engineerName,
+                answers.engineerId,
+                answers.engineerEmail,
+                answers.engineerGithub
+            );
+            teamMembers.push(engineer);
+            idArray.push(answers.engineerId);
+            creatTeam();
+        });
+    }
+    function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'internName',
+                    message: "What is the interns's name?",
+                    validate: (answer) => {
+                        if (answer !== '') {
+                            return true;
+                        }
+                            return "Please enter at least on character.";
+                    },
+                },{
+                    type: 'input',
+                    name: 'internId',
+                    message: "what is the intern's ID number? ",
+                    validate: (answer) => {
+                        const pass = answer.match(/^[1-9]\d*$/)
+                        if (pass) {
+                            return true
+                        }
+                            return 'please enter a positive number greater than zero.'
+                    },
+                },{
+                    
+                        type: 'input',
+                        name: 'internEmail',
+                        message: "what is the intern's email?",
+                        validate: (answer) => {
+                            const pass = answer.math(/\S+@\S+\.\S+/)
+                            if (pass) {
+                                return true
+                            }
+                                return 'please enter a vazlid email address.';
+                    },
+                },{
+                    type: 'input',
+                    name: 'internSchool',
+                    message: "What is this interns School? ",
+                    validate: (answer) => {
+                        if (answer !== '') {
+                            return true
+                        }
+                            return 'please enter a positive number greater than zero.'
+                    },
+                },
+        ])
+        .then((answers) => {
+            const intern = new Intern(
+                answers.internName,
+                answers.internId,
+                answers.internEmail,
+                answers.internSchool
+            );
+            teamMembers.push(intern);
+            idArray.push(answers.internID);
+            creatTeam();
+        });
+    }
+
+    function buildTeam() {
+
+        if (!fs.existsSync(DIST_DIR)) {
+            fs.mkdirSync(DIST_DIR);
+        }
+        fs.writeFileSync(distpath, render(teamMembers), 'utf-8');
+    }
+
+    creatManager();
 }
+
+appMenu();
--- src/page-template.js
+++ src/page-template.js
@@ -18,5 +18,61 @@ const generateTeam = team => {
    `;
 };
 
+const generateEngineer = engineer => {
+    return `
+    <div class="card" employee-card">
+    <div class="card-header">
+        <h3 class="card-title">${engineer.getName()}</h3>
+        <h4 class="card-title">${engineer.getRole()}</h4>
+    </div>
+    <div class='card-body'>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${engineer.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+`;
+};
 
-}
+const generateIntern = intern => {
+    return `
+    <div class="card" employee-card">
+    <div class="card-header">
+        <h3 class="card-title">${intern.getName()}</h3>
+        <h4 class="card-title">${intern.getRole()}</h4>
+    </div>
+    <div class='card-body'>
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${intern.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+`;
+};
+
+    const html = [];
+    html.push(team
+        .filter(employee => employee.getRole() === 'Manger')
+        .map(manager => generateManager(manager))
+        );
+    html.push(team
+        .filter(employee => employee.getRole() === 'Engineer')
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+        );
+    html.push(team
+        .filter(employee => employee.getRole() === 'Intern')
+        .map(intern => generateIntern(intern))
+        .join("")
+        );
+        
+        return html.join("");
+    }
+
+    module.exports = team => {
+        
+    }
